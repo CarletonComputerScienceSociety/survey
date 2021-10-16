6170d5619a76511b2d76ad4d6e6467bdd868e2f4
Matthew M-B <matthewmacraebovell@outlook.com>
2021-10-16 12:04:05 -0400

    add backend django api (#4)

---
+++ api/polls/urls.py
@@ -0,0 +1,26 @@
+from django.urls import include, path
+from django.conf.urls import url
+from rest_framework import routers
+from .views import *
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Polls API",
+        default_version="v1",
+        description="tbd",
+        terms_of_service="(Top Be Updated)",  # To be Changed
+        contact=openapi.Contact(email="contact@ccss.com"),  # To be Changed
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
+urlpatterns = [
+    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+]
