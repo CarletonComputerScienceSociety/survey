9c612d83e562a732d7cab264934b152260423d3b
Dhirran <68429657+Dhirru12@users.noreply.github.com>
2021-11-04 12:28:14 -0400

    Added Question Models (#5)

--- api/polls/admin.py
+++ api/polls/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Question)
+class QuestionResource(admin.ModelAdmin):
+    list_display = ["id", "body"]
+
+
+@admin.register(Poll)
+class PollResource(admin.ModelAdmin):
+    list_display = ["id", "description"]
+
+
+@admin.register(MultipleChoiceQuestion)
+class MultipleChoiceQuestionResource(admin.ModelAdmin):
+    list_display = ["id"]
+
+
+@admin.register(MultipleChoiceAnswer)
+class MultipleChoiceAnswerResource(admin.ModelAdmin):
+    list_display = ["id", "answer_body", "multiple_choice_question"]
+
+
+@admin.register(WrittenQuestion)
+class WrittenQuestionResource(admin.ModelAdmin):
+    list_display = ["id"]
--- api/polls/models.py
+++ api/polls/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    title = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200)
+
+
+class Question(models.Model):
+    body = models.CharField(max_length=200)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, null=True)
+
+
+class MultipleChoiceQuestion(Question):
+    def __str__(self):
+        return self.id
+
+
+class MultipleChoiceAnswer(models.Model):
+    answer_body = models.CharField(max_length=200)
+    multiple_choice_question = models.ForeignKey(
+        MultipleChoiceQuestion,
+        related_name="questions",
+        on_delete=models.CASCADE,
+        null=True,
+    )
+
+
+class WrittenQuestion(Question):
+    def __str__(self):
+        return self.id
