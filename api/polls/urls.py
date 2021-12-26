f44262dcdd46af0ffd4dfcd9fbbaa874c3a9d7a7
Matthew M-B <matthewmacraebovell@outlook.com>
2021-12-26 13:25:44 -0500

    Added poll endpoints (#8)
    Co-authored-by: Dhirru12 <dhirru.p12@gmail.com>

--- api/polls/urls.py
+++ api/polls/urls.py
@@ -6,6 +6,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Polls API",
@@ -23,4 +24,9 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path(
+        "poll/<id>",
+        PollDetails.as_view(),
+        name="poll-details",
+    ),
 ]
--- api/polls/views.py
+++ api/polls/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.db.models.query import QuerySet
+from rest_framework import status, generics
+from rest_framework.response import Response
+from .models import *
+from django.db import models
+from rest_framework import serializers
+from uuid import UUID
+from .serializers import *
 
-# Create your views here.
+
+# Details of selected Organization
+class PollDetails(generics.RetrieveAPIView):
+
+    serializer_class = PollSerializer
+
+    def get(self, request, id):
+        try:
+            poll = Poll.objects.get(id=id)
+            serializer = PollSerializer(poll, many=False)
+            final_data = {"data": serializer.data, "errors": None}
+            return Response(final_data, status=status.HTTP_200_OK)
+        except Poll.DoesNotExist:
+            final_data = {"data": {}, "errors": "Event not found"}
+            return Response(final_data, status=status.HTTP_404_NOT_FOUND)
