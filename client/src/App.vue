7c51be70e360b7e8917bba87f21f1bd45c80250a
Dhirru12 <dhirru.p12@gmail.com>
2022-03-24 12:51:02 -0400

    test

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,59 +1,45 @@
 <template>
-  <!-- <div id="nav">
-    <router-link to="/">Survey</router-link> |
-    <router-link to="/about">About</router-link>
-  </div> -->
-  <router-view />
-  <div class="footer">© 2021 Carleton Computer Science Society</div>
+  <div id="app">
+    <pie-chart :data="chartData" :options="chartOptions"></pie-chart>
+  </div>
 </template>
 
-<style lang="scss">
-body {
-  padding: 0;
-  margin: 0;
-}
-
-.survey-page {
-  height: 90vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  .survey-complete-message {
-    font-size: 32px;
-    font-weight: 700;
+<script>
+import PieChart from "./views/PieChart.js";
+export default {
+  name: "App",
+  components: {
+    PieChart
+  },
+  data() {
+    return {
+      chartOptions: {
+        hoverBorderWidth: 20
+      },
+      chartData: {
+        hoverBackgroundColor: "red",
+        hoverBorderWidth: 10,
+        labels: ["Green", "Red", "Blue"],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: ["#41B883", "#E46651", "#00D8FF"],
+            data: [1, 10, 5]
+          }
+        ]
+      }
+    };
   }
-}
+};
+</script>
 
-.footer {
-  height: 10vh;
-  background: black;
-  color: white;
-  display: flex;
-  font-size: 0.9rem;
-  justify-content: center;
-  align-items: center;
-  border-top: 3px solid #c40729;
-}
-
+<style>
 #app {
-  font-family: "Source Sans Pro", sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
--- client/src/views/Statistics.vue
+++ client/src/views/Statistics.vue
@@ -1,82 +1,45 @@
 <template>
-  <!--<div class="survey-page" v-if="!this.loading() && this.errorMessage === null" > {{JSON.stringify(this.response)}}</div>-->
-  <div class="small">
-    <bar-chart :chart-data=createChart(this.response[0])></bar-chart>
+  <div id="app">
+    <pie-chart :data="chartData" :options="chartOptions"></pie-chart>
   </div>
 </template>
+
 <script>
-import { getStatistics } from "@/services";
-import { Bar } from 'vue-chartjs'
+import PieChart from "./PieChart.js";
 export default {
-  name: "StatisticsPage",
-  components: {Bar},
+  name: "App",
+  components: {
+    PieChart
+  },
   data() {
     return {
-      response: null,
-      statistics: null,
-      errorMessage: null,
-    };
-  },
-  async created() {
-    try {
-      let response = await this.getData(this.$route.params.id);
-      this.initData(response.statistics);
-    } catch (error) {
-      console.log(error);
-      this.errorMessage = error;
-    }
-  },
-  methods: {
-    getData: async (id) => {
-      return await getStatistics(id);
-    },
-    initData(response) {
-      this.response = response;
-    },
-    loading() {
-      return this.response === null;
-    },
-    error() {
-      console.log(this.response);
-      return false;
-    },
-    
-    createChart(stats){
-      const values = {};
-
-      for(var i = 0; i < stats[0].length; i++) {
-        values.push(stats[0][stats[0].keys[i]])
-      }
-      console.log(values)
-      const data = {
-        labels: stats[0].keys,
+      chartOptions: {
+        hoverBorderWidth: 20
+      },
+      chartData: {
+        hoverBackgroundColor: "red",
+        hoverBorderWidth: 10,
+        labels: ["Green", "Red", "Blue"],
         datasets: [
           {
-            label: 'Dataset 1',
-            data: stats[0],
-            //backgroundColor: Object.values(Utils.CHART_COLORS),
+            label: "Data One",
+            backgroundColor: ["#41B883", "#E46651", "#00D8FF"],
+            data: [1, 10, 5]
           }
         ]
-      };
-      return {
-        type: 'bar',
-        data: data,
-        options: {
-          responsive: true,
-         plugins: {
-           legend: {
-             position: 'top',
-           },
-           title: {
-             display: true,
-              text: 'Chart.js Pie Chart'
-            }
-          }
-        },
-      };
-    }
-
-    
-  },
+      }
+    };
+  }
 };
 </script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
